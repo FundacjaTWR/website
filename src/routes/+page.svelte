ee59a794fe8eeaba132e99e9c75c32f0128d9fdc
MrTalon63 <marcin@mrtalon.eu>
2024-06-07 13:06:35 +0200

    Placeholder

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import logo from '$lib/assets/logo_medium.webp';
+</script>
+
+<div class="bg-color-red container bg-gray-200 min-h-screen min-w-full">
+	<div class="justify-center text-center space-y-2">
+		<img src={logo} alt="Fundacja Transpłciowość W Rodzinie logo" class="w-1/4 mx-auto" />
+		<h1 class="font-semibold text-4xl">Fundacja Transpłciowość W Rodzinie</h1>
+		<h2 class="text-2xl">Strona w budowie</h2>
+		<p>m.czop [at] fundacjatransplciowoscwrodzinie.pl</p>
+	</div>
+</div>
