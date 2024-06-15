40a62529fcfc7aa548dc2feddacecd0048d85b8c
Marcin Czop <marcin@mrtalon.eu>
2024-06-15 14:27:20 +0200

    Update email address

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,6 @@
 		<img src={logo} alt="Fundacja Transpłciowość W Rodzinie logo" class="w-1/4 mx-auto" />
 		<h1 class="font-semibold text-4xl">Fundacja Transpłciowość W Rodzinie</h1>
 		<h2 class="text-2xl">Strona w budowie</h2>
-		<p>m.czop [at] fundacjatransplciowoscwrodzinie.pl</p>
+		<p>m.czop [at] ftwr.org.pl</p>
 	</div>
 </div>
